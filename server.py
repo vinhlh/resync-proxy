d8f039700fbe736e72ca831eea63483bc63bd7ff
Vinh Le <vinh.le@vn.zalora.com>
2016-03-28 17:14:20 +0700

    add logger

--- server.py
+++ server.py
@@ -6,18 +6,24 @@
 
 import tornado.ioloop
 import tornado.web
+from tornado.options import options
 
 import config
 from src.ResyncHandler import ResyncHandler
 from src.HealthCheckHandler import HealthCheckHandler
 
-def make_app():
+def makeApp():
     return tornado.web.Application([
         (r'/health-check/([a-z-]+)', HealthCheckHandler),
         (r'/resync/([a-zA-Z0-9:\.=-\\s]+)', ResyncHandler)
     ])
 
 if __name__ == '__main__':
-    app = make_app()
+    app = makeApp()
+
+    # add logger
+    options.log_file_prefix = config.log
+    options.parse_command_line()
+
     app.listen(config.port)
     tornado.ioloop.IOLoop.current().start()
